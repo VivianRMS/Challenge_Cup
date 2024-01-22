8c3e97f4fbd1a5ec35ae0e28163a253bf481475a
K0-R <vivianrklds221228@gmail.com>
2024-01-22 13:34:42 -0500

    Update MAR.py

--- MAR.py
+++ MAR.py
@@ -16,3 +16,15 @@ def mouth_aspect_ratio(mouth):
 
     # return the mouth aspect ratio
     return mar
+
+def mouth_upper_ratio(mouth):
+    A = dist.euclidean(mouth[50], mouth[60])
+    B = dist.euclidean(mouth[60], mouth[64])
+    mur = A / B
+    return mur
+
+def mouth_lower_ratio(mouth):
+    A = dist.euclidean(mouth[55], mouth[64])
+    B = dist.euclidean(mouth[60], mouth[64])
+    mlr = A / B
+    return mlr
